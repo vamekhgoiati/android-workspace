0e1de77a1e41425957dcfbc76aa9e940a90e3abe
vamekhgoiati <vamekh.goiati@gmail.com>
2015-05-13 21:10:22 +0400

    added cursor close, broadcast and alarm service start/stop

--- GeoCoordinateHistory/src/com/myProjects/geocoordinatehistory/MyGeoHistoryActivity.java
+++ GeoCoordinateHistory/src/com/myProjects/geocoordinatehistory/MyGeoHistoryActivity.java
@@ -8,7 +8,9 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +18,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MyGeoHistoryActivity extends Activity {
+	
+	private static final String TAG = MyGeoHistoryActivity.class.getSimpleName();
 	
 	private static final Long ALARM_DELAY = 10 * 1000L;
 	private AlarmManager mAlarmManager;
@@ -25,16 +29,19 @@ public class MyGeoHistoryActivity extends Activity {
 	private Button mStartButton;
 	private Button mStopButton;
 	private Button mDrawMapButton;
-	private AlarmReciever mAlarmReciever;
+	private Button mClearButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		mDbHelper = new DatabaseHelper(getApplicationContext());
+		
 		mStartButton = (Button) findViewById(R.id.start_button);
 		mStopButton = (Button) findViewById(R.id.stop_button);
 		mDrawMapButton = (Button) findViewById(R.id.draw_map);
+		mClearButton = (Button) findViewById(R.id.clear_button);
 		
 		mStartButton.setOnClickListener(new OnClickListener() {
 			
@@ -52,8 +59,51 @@ public class MyGeoHistoryActivity extends Activity {
 			}
 		});
 		
+		mDrawMapButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				drawRoute();
+			}
+		});
+		
+		mClearButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				clearDbContent();
+			}
+		});
+		
 	}
 	
+	private void clearDbContent() {
+		mDbHelper.getWritableDatabase().delete(DatabaseHelper.TABLE_NAME, null, null);
+	}
+
+	private void drawRoute() {
+		
+		Cursor c = readLocations();
+		double latitude;
+		double longtitude;
+		
+		if (c.moveToFirst()) {
+			do {
+				latitude = c.getDouble(c.getColumnIndex(DatabaseHelper.LATITUDE));
+				longtitude = c.getDouble(c.getColumnIndex(DatabaseHelper.LONGTITUDE));
+				Log.d(TAG, "Latitude : " + latitude + "; Longtitude : " + longtitude);
+			} while (c.moveToNext());
+		}
+		
+		c.close();
+		
+	}
+
+	private Cursor readLocations() {
+		return mDbHelper.getWritableDatabase()
+				.query(DatabaseHelper.TABLE_NAME, DatabaseHelper.columns, null, new String[] {}, null, null, DatabaseHelper.ID);
+	}
+
 	private void enableBroadcastService(){
 		
 		ComponentName reciever = new ComponentName(this, AlarmReciever.class);
@@ -67,14 +117,22 @@ public class MyGeoHistoryActivity extends Activity {
 		
 		mAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + ALARM_DELAY, ALARM_DELAY, mGPSCoordinatePendingintent);
 		
+		Log.i(TAG, "Alarm service started, Broadcast service enabled.");
+		
 	}
 	
 	private void disableBroadcastService(){
+		
+		if (mAlarmManager != null && mGPSCoordinatePendingintent != null) {
+			mAlarmManager.cancel(mGPSCoordinatePendingintent);
+			Log.i(TAG, "Alarm service canceled.");
+		}
 		
 		ComponentName reciever = new ComponentName(this, AlarmReciever.class);
 		
 		PackageManager pm = getPackageManager();
 		pm.setComponentEnabledSetting(reciever, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+		Log.i(TAG, "Broadcast disabled.");
 		
 	}
 	
